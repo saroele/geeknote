68a2d7c705b1af2d6ad876558509c6aea36e52d8
Dale Jung <dale@dalejung.com>
2013-09-28 13:23:58 -0400

    prelim setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup                                                      
+                                                                                      
+DISTNAME='geeknote'                                                                 
+FULLVERSION='0.1'                                                                     
+                                                                                      
+setup(name=DISTNAME,                                                                  
+      version=FULLVERSION,                                                            
+      packages=['geeknote',
+               ]                                                         
+      )   
